477dd74f58d8472c4ae2b0077d9d87e836234887
leonardoantonelli <antonellileonardodesign@gmail.com>
2023-03-20 15:12:40 +0100

    correction

--- progettoVener/src/progettoVener/Main.java
+++ progettoVener/src/progettoVener/Main.java
@@ -6,12 +6,12 @@ public class Main {
 	//creo array di elementimultmediali con 5
  
 	static Scanner scanner= new Scanner(System.in);
- static ElementoMultimediale[] elementimultimediali = new ElementoMultimediale[5];	
+ static ElementoMultimediale[] elementimultimediali = new ElementoMultimediale[2];	
  
   public static void main (String [] args) {
 	
 	creaElemento();
-	//play();
+	play();
 
 }
 	
@@ -22,7 +22,7 @@ private static void creaElemento() {
 		System.out.println("Scrivi 1 per Immagine, 2 per Video, 3 per Registrazione Audio): ");
 	
 	 int scelta = scanner.nextInt();
-	  scanner.nextLine();
+	scanner.nextLine();
             
             switch (scelta) {
             //caso1 immagine
@@ -44,10 +44,13 @@ private static void creaElemento() {
                 String titolovideo = scanner.nextLine();
                System.out.println("Inserisci la luminosità:");
                 int luminositavideo = scanner.nextInt();
+                scanner.nextLine();
                 System.out.println("Inserisci il volume:");
                 int volumevideo = scanner.nextInt();
+                scanner.nextLine();
                 System.out.println("Inserisci la durata:");
                 int duratavideo = scanner.nextInt();
+                scanner.nextLine();
                             
                 elementimultimediali[i] = new video(titolovideo,luminositavideo, duratavideo, volumevideo);
                 break;
@@ -58,8 +61,10 @@ private static void creaElemento() {
             	
             	System.out.println("Inserisci la durata:");
             	int durataAudio=scanner.nextInt();
+            	scanner.nextLine();
             	System.out.println("volume dell'audio");
             	int volumeAudio = scanner.nextInt();
+            	scanner.nextLine();
             
             //ora che ho tutti i dati mi creo elemento 
             	elementimultimediali[i]= new audio(titoloAudio, volumeAudio,durataAudio);
@@ -74,11 +79,11 @@ private static void creaElemento() {
 	}
 }
 
-public static void readFiles() {
-    Scanner input = new Scanner(System.in);
-    System.out.println("se vuoi sapere quali sono i file che hai salvato premi 1\nse salvare nuovi file premi 2\nse vuoi leggere un singolo file premi 3");
-    int e = input.nextInt();
-    input.nextLine();
+public static void play() {
+    
+    System.out.println("se vuoi sapere quali sono i file che hai salvato premi 1\nse vuoi fare play premi 2");
+    int e = scanner.nextInt();
+    scanner.nextLine();
     switch (e) {
         case 1:
             for (int i = 0; i < elementimultimediali.length; i++) {
@@ -87,7 +92,8 @@ public static void readFiles() {
             break;
         
         case 2:
-            System.out.println("da settare");
+        	for (int i = 0; i < elementimultimediali.length; i++) {
+                System.out.println(elementimultimediali[i]);
             break;
     }
 }
